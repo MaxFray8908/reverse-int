4b97a6706f391273ec2d9d4e49c48a37d181f3d0
MaxFray8908 <nadja.gawrilowa@yandex.ru>
2022-01-26 13:54:34 +0000

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let strNumber = String(n);
+    let result = '';
+    for(let i = strNumber.length - 1; i >= 0; i--) {
+        if (strNumber[i] !== '-'){
+            result += strNumber[i];
+        }
+    }
+    return +result;
 }
